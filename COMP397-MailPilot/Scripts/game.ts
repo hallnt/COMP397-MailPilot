13243fc44be52e569271415315a8728f317e7826
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-06-16 10:58:13 -0400

    Cleaned up BoilerPlate

--- COMP397-MailPilot/Scripts/game.ts
+++ COMP397-MailPilot/Scripts/game.ts
@@ -12,14 +12,14 @@ var stats: Stats;
 
 var assets: createjs.LoadQueue;
 var manifest = [
-    { id: "pinkButton", src: "assets/images/pinkButton.png" },
-    { id: "clicked", src: "assets/audio/clicked.wav" }
+    { id: "plane", src: "assets/images/plane.png" }
+
 ];
 
 
 // Game Variables
-var helloLabel: createjs.Text; // create a reference
-var pinkButton: createjs.Bitmap;
+
+// NEED plane object
 
 
 // Preloader Function
@@ -75,33 +75,8 @@ function pinkButtonClicked(event: createjs.MouseEvent) {
 
 // Callback functions that change the alpha transparency of the button
 
-// Mouseover event
-function pinkButtonOver() {
-    pinkButton.alpha = 0.8;
-}
-
-// Mouseout event
-function pinkButtonOut() {
-    pinkButton.alpha = 1.0;
-}
 
 // Our Main Game Function
 function main() {
-    console.log("Game is Running");
-    helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
-    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-    helloLabel.x = 160;
-    helloLabel.y = 190;
-    stage.addChild(helloLabel);
 
-    pinkButton = new createjs.Bitmap(assets.getResult("pinkButton"));
-    pinkButton.regX = pinkButton.getBounds().width * 0.5;
-    pinkButton.regY = pinkButton.getBounds().height * 0.5;
-    pinkButton.x = 160;
-    pinkButton.y = 270;
-    stage.addChild(pinkButton);
-    pinkButton.on("click", pinkButtonClicked);
-    pinkButton.on("mouseover", pinkButtonOver);
-    pinkButton.on("mouseout", pinkButtonOut);
 }
